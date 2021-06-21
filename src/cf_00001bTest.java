e7a127bb5f8c23f4ddf768b3591c8a016a021d05
areful <areful@yandex.ru>
2021-06-21 19:20:21 +0300

    Initial commit

---
+++ src/cf_00001bTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class cf_00001bTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void generate() {
+        generate()
+    }
+
+    @Test
+    void getDecode() {
+    }
+}

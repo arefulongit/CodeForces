48ef09bbb8c0aaf63f9862d728c3ec7920809737
areful <areful@yandex.ru>
2021-07-08 23:13:38 +0300

    ALL WORKS

---
+++ src/CF00001B.java
@@ -0,0 +1,138 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+/**
+ * В популярных системах электронных таблиц (например, в программе Excel) используется следующая нумерация колонок.
+ * Первая колонка имеет номер A, вторая B и т.д. до 26-ой, которая обозначается буквой Z.
+ * Затем идут двухбуквенные обозначения: 27-ая обозначается как AA, 28-ая как AB, а 52-я обозначается парой AZ.
+ * Аналогично, следом за ZZ следуют трехбуквенные обозначения и т.д.
+ * Строки обычно обозначаются целыми числами от 1. Номер ячейки получается конкатенацией обозначений для столбца и строки.
+ * Например, BC23 это обозначение для ячейки в столбце 55, строке 23.
+ * Иногда используется другая форма записи: RXCY, где X и Y это целые числа, обозначающие номер строки и столбца,
+ * соответственно. Например, R23C55 это ячейка из примера выше.
+ * Ваша задача написать программу, которая считывает последовательность обозначений ячеек и выводит каждое из обозначений
+ * в другой форме записи.
+ * Входные данные
+ * В первой строке записано целое число n (1 ≤ n ≤ 105), количество обозначений в тесте. Далее идет n строк,
+ * каждая из которых содержит обозначение. Известно, что все обозначения корректны, и не содержат ячейки с номерами строк
+ * или столбцов больших 106.
+ * Выходные данные
+ * Выведите n строк — каждая строка должна содержать обозначение ячейки в отличной форме записи.
+ * Примеры
+ * входные данныеСкопировать
+ * 2
+ * R23C55
+ * BC23
+ * выходные данныеСкопировать
+ * BC23
+ * R23C55
+ */
+
+
+public class CF00001B {
+
+    static final char[] CHARS = new char[]{
+            'Z', 'A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y'
+    };
+
+    static final int BASE = CHARS.length;
+
+    static final int MAX_CHAR_QUO = 26;
+
+    static final int[] ASC_N = {
+            1,
+            MAX_CHAR_QUO,
+            MAX_CHAR_QUO * MAX_CHAR_QUO,
+            MAX_CHAR_QUO * MAX_CHAR_QUO * MAX_CHAR_QUO,
+            MAX_CHAR_QUO * MAX_CHAR_QUO * MAX_CHAR_QUO * MAX_CHAR_QUO,
+            MAX_CHAR_QUO * MAX_CHAR_QUO * MAX_CHAR_QUO * MAX_CHAR_QUO * MAX_CHAR_QUO
+    };
+
+    public static void main(String[] args) throws IOException {
+        String[] strs = null;
+        CF00001B cf = new CF00001B();
+        Scanner sc = new Scanner(System.in);
+        int times = sc.nextInt();
+        strs = new String[times];
+        for (int c = 0; c < times; c++) {
+            strs[c] = sc.next();
+        }
+        for (int c = 0; c < times; c++) {
+            System.out.println(cf.sort(strs[c]));
+        }
+    }
+
+    public String sort(String pStr) {
+        Function<String, String> converterFunction = null;
+        Pattern rcPattern = Pattern.compile("^R[0-9]{1,10}C[0-9]{1,10}$");
+        Pattern a1Pattern = Pattern.compile("^[A-Z]{1,10}[0-9]{1,10}$");
+        if (a1Pattern.matcher(pStr).find()) {
+            converterFunction = convertA1ToRC;
+        } else if (rcPattern.matcher(pStr).find()) {
+            converterFunction = convertRCToA1;
+        }
+        return converterFunction.apply(pStr);
+    }
+
+    Function<String, String> convertRCToA1 = pStringForConversion -> {
+        String[] strs = pStringForConversion
+                .replace("R", "")
+                .split("C");
+        String rowNumber = strs[0];
+        String columnNumber = strs[1];
+        String columnName = getColumnNameByColumnNumber(columnNumber);
+        return columnName + rowNumber;
+    };
+
+    Function<String, String> convertA1ToRC = pStringForConversion -> {
+        String letters = new StringBuilder(
+                pStringForConversion
+                        .replaceAll("[0-9]", "")
+        ).reverse().toString();
+        String digits = pStringForConversion.replaceAll("[A-Z]", "");
+        int indexInN = 0;
+        int columnNumber = 0;
+        for (char letter : letters.toCharArray()) {
+            int letterIndexInChars = Arrays.binarySearch(CHARS, letter);
+            columnNumber = columnNumber + ASC_N[indexInN] * letterIndexInChars;
+            indexInN++;
+        }
+        return "R" + digits + "C" + columnNumber;
+    };
+
+    public String getColumnNameByColumnNumber(String pColumnNumber) {
+        int columnNumber = Integer.parseInt(pColumnNumber);
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            if (columnNumber < BASE) {
+                sb.append(CHARS[columnNumber]);
+                break;
+            }
+            int frequent = columnNumber / BASE;
+            int remainder = columnNumber % BASE;
+            if (remainder == 0) {
+                if (columnNumber >= BASE) {
+                    columnNumber = frequent - 1;
+                    if (columnNumber == 0) {
+                        sb.append(CHARS[remainder]);
+                        break;
+                    }
+                } else {
+                    columnNumber = frequent;
+                }
+            } else {
+                columnNumber = frequent;
+            }
+            sb.append(CHARS[remainder]);
+        }
+        return sb.reverse().toString();
+    }
+}
---
+++ src/CF00001BTest.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class CF00001BTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void generate() {
+        CF00001B cf = new CF00001B();
+    }
+
+    @Test
+    void getDecode() {
+    }
+
+     @Test
+    void getColumnNameByColumnNumber() {
+        CF00001B cf = new CF00001B();
+         String exp1 = "A";
+         String act1 = cf.getColumnNameByColumnNumber("1");
+         Assert.assertEquals(exp1,act1);
+        String exp2 = "Z";
+        String act2 = cf.getColumnNameByColumnNumber("26");
+        Assert.assertEquals(exp2,act2);
+         String exp3 = "AA";
+         String act3 = cf.getColumnNameByColumnNumber("27");
+         Assert.assertEquals(exp3,act3);
+         String exp4 = "AZ";
+         String act4 = cf.getColumnNameByColumnNumber("52");
+         Assert.assertEquals(exp4,act4);
+    }
+
+    @Test
+    void sort() throws Exception {
+        CF00001B cf = new CF00001B();
+        String exp1 = "R23C55";
+        String act1 = cf.sort("BC23");
+        Assert.assertEquals(exp1,act1);
+        String exp2 = "RZ228";
+        String act2 = cf.sort("R228C494");
+        Assert.assertEquals(exp2,act2);
+    }
+}
